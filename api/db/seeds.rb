cdd1e93f4ebbec8414c8590799abb463ee51efdd
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-12-23 21:23:16 -0500

    Add test user for development.

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
 Section.create!(title: 'General') unless Section.any?
+
+if Rails.env.development?
+  User.find_or_create_by!(email: 'test@development.com') do |user|
+    user.display_name = 'Test User'
+    user.credentials << UserCredential::Password.new(password: 'password')
+  end
+end
