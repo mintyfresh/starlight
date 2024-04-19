7e6e7a446f2991da985844bf9b5ae9b5ddb0e930
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2024-04-18 23:08:40 -0400

    Preselect the user's timezone as the default

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'popper'
 import 'bootstrap'
+
+document.addEventListener('DOMContentLoaded', function() {
+  // find all elements with the data-default-value="current-time-zone" attribute
+  document.querySelectorAll('[data-default-value="current-time-zone"]').forEach((element) => {
+    if (!element.value) {
+      element.value = Intl.DateTimeFormat().resolvedOptions().timeZone
+    }
+  })
+})
